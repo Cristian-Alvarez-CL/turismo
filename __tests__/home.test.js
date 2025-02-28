2f16aecd3743292e69d7c630f7ac62c84814ff22
Cristian Alvarez <cristianalvarez@MacBook-Pro-de-Cristian.local>
2025-02-28 15:54:46 -0300

    feat: Se modifica test

--- __tests__/home.test.js
+++ __tests__/home.test.js
@@ -1,14 +1,6 @@
 import { render, screen } from "@testing-library/react"
 import Home from "../app/page"
 
-jest.mock("next/image", () => ({
-  __esModule: true,
-  default: (props) => {
-    // eslint-disable-next-line @next/next/no-img-element
-    return <img {...props} alt={props.alt} />
-  },
-}))
-
 describe("Home", () => {
   it("renders a heading", () => {
     render(<Home />)
--- jest.config.js
+++ jest.config.js
@@ -1,13 +1,21 @@
-module.exports = {
-  testEnvironment: "jsdom",
+const nextJest = require("next/jest")
+
+const createJestConfig = nextJest({
+  // Provide the path to your Next.js app to load next.config.js and .env files in your test environment
+  dir: "./",
+})
+
+// Add any custom config to be passed to Jest
+const customJestConfig = {
   setupFilesAfterEnv: ["<rootDir>/jest.setup.js"],
+  testEnvironment: "jest-environment-jsdom",
   moduleNameMapper: {
+    // Handle module aliases (if you're using them in your project)
     "^@/components/(.*)$": "<rootDir>/components/$1",
     "^@/pages/(.*)$": "<rootDir>/pages/$1",
     "^@/app/(.*)$": "<rootDir>/app/$1",
   },
-  testPathIgnorePatterns: ["<rootDir>/.next/", "<rootDir>/node_modules/"],
-  transform: {
-    "^.+\\.(js|jsx|ts|tsx)$": ["babel-jest", { presets: ["next/babel"] }],
-  },
 }
+
+// createJestConfig is exported this way to ensure that next/jest can load the Next.js config which is async
+module.exports = createJestConfig(customJestConfig)
