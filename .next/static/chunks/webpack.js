5534834a5b0e3bf6e5f8409473d56fe6b3d3b852
Cristian Alvarez <cristianalvarez@MacBook-Pro-de-Cristian.local>
2025-02-28 13:15:47 -0300

    feat: Se modifica pipeline en github actions

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -192,7 +192,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "d466cff20ccbeaa7"; }
+/******/ 		__webpack_require__.h = function() { return "bf8dd1d98b0a08e8"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
